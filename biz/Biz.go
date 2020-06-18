7fdad80479bedba0887711e118fa7f661e443023
xinxi.li <xinxi.li@dianping.com>
2020-06-18 22:08:43 +0800

    fb;bid

---
+++ biz/Biz.go
@@ -0,0 +1,17 @@
+package biz
+
+import (
+	"github.com/KunBetter/BizFrame/core"
+)
+
+type FeedBack struct {
+}
+
+func (fb *FeedBack) Handle(request core.Request, session core.Session) {
+}
+
+type Bid struct {
+}
+
+func (bid *Bid) Handle(request core.Request, session core.Session) {
+}
